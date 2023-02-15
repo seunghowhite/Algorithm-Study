fd8dfb5187a1f479b88c62e4374ecda75ff76f46
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2023-02-15 10:17:20 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,22 @@
+function solution(a, b) {
+    var answer = 0;
+    if(a==b){
+        
+        answer=a
+        
+    }else if(a<b){
+        
+        for(let i=a;i<=b;i++){
+        answer+=i
+        }
+        
+    }else{
+        
+        for(let i=b;i<=a;i++){
+        answer+=i
+        }
+        
+    }
+    
+    return answer;
+}
