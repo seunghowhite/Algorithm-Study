98bb52af2a2bf41b9854b4308ff4e126f0c40eed
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2023-02-15 13:32:51 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -0,0 +1,12 @@
+function solution(a, b) {
+    // 윤년은 2월이 29일까지 있고, 1년이 366일이다.
+    // 4, 100, 400으로 나누어지는 년도는 윤년이다.
+    const weekToDay = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
+    const leapYearMonths = [31,29,31,30,31,30,31,31,30,31,30,31]; 
+    let day = b+4; // 1월 1일은 FRI, b가 1일 때 인덱스로는 5가 되어야한다.
+    // 1월은 이전 월의 일수를 더하지 않음으로 a-1 조건으로 해야한다.
+    for(let i = 0; i < a-1; ++i){
+        day += leapYearMonths[i];
+    }
+    return weekToDay[day%7];//7일로 나눠서 남은 나머지 값이 요일이다.
+}
