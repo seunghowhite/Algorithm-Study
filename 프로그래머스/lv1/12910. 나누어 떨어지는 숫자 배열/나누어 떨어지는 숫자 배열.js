ee1f525ea9a7bf85ffa9a20037bdd0b716f8cdf0
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2023-02-17 11:47:18 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.15 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
+++ 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
@@ -1,22 +1,35 @@
 function solution(arr, divisor) {
-    var answer =[];
-    var sum=[]
-//     arr각각 값이<for> div로 나누어 떨어질때
-//   그 값을 반환 한다.
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]%divisor==0 || divisor==1){
-            sum.push(arr[i]);
-        }
-    }
-    if(sum.length==0){
-        sum.push(-1);
-    }
-    
-//     이제 배열을 오름 차순 정렬
-    sum.sort(function(a,b){
-        if (a > b) return 1
-        else if (b > a) return -1;
-        else return 0;
+    var answer = [];
+    arr.map((a) => {
+        a % divisor === 0 && answer.push(a);
     })
-    return sum;
+    return answer.length ? answer.sort((a, b) => a - b) : [-1];
+
 }
+
+
+
+
+// 예전코드
+// function solution(arr, divisor) {
+//     var answer =[];
+//     var sum=[]
+// //     arr각각 값이<for> div로 나누어 떨어질때
+// //   그 값을 반환 한다.
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i]%divisor==0 || divisor==1){
+//             sum.push(arr[i]);
+//         }
+//     }
+//     if(sum.length==0){
+//         sum.push(-1);
+//     }
+    
+// //     이제 배열을 오름 차순 정렬
+//     sum.sort(function(a,b){
+//         if (a > b) return 1
+//         else if (b > a) return -1;
+//         else return 0;
+//     })
+//     return sum;
+// }
