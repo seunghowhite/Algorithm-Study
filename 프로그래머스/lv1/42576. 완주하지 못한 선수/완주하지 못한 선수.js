b701adf77f8354a6aa7780d814e6d31dfb65fb42
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2023-02-17 15:40:31 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 72.55 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,15 +1,19 @@
 function solution(participant, completion) {
-    var answer ='';
-    participant.sort();
-    completion.sort();
-    for(let i=0; i<participant.length;i++){
-         if(participant[i]!==completion[i]){
-             return participant[i]
-         }
+    var answer = '';
+    
+    let objpar =participant.reduce((acc,cur)=>{
+         acc[cur] = (acc[cur] || 0) + 1;
+        return acc;
+    },{});
+    
+    completion.forEach((person)=>objpar[person]--)
+    
+    
+    for(let person in objpar){
+       if(objpar[person]!==0){
+           return person
+       }     
     }
-    
-    
-    return answer;
 }
 
 
@@ -17,24 +21,15 @@ function solution(participant, completion) {
 
 
 
-
-
-
-
-
-
-
+// 그전 코드
 // function solution(participant, completion) {
-//     var answer = '';
-// //     map을 사용해 이름이 같은게 있으면 value값에 +1해주자.
-//     let myObj =participant.map(part=>part+1)
-//     console.log( '결과값★:', myObj)
-
-// //     map을 사용해서 completion의 요소와 이름이 같으면 value값에 -1을 해주자.
-    
-// //     map을 사용해서 0인 값들을 가지고 오자.
-
-    
-// //     그리고 출력하자.
+//     var answer ='';
+//     participant.sort();
+//     completion.sort();
+//     for(let i=0; i<participant.length;i++){
+//          if(participant[i]!==completion[i]){
+//              return participant[i]
+//          }
+//     }
 //     return answer;
 // }
