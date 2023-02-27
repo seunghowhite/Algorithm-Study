2a5e564eee02621ae94f451fde44271a23205f06
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2023-02-27 09:32:54 +0900

    [level 0] Title: 암호 해독, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
@@ -1,12 +1,20 @@
 function solution(cipher, code) {
-    var answer = [];
-    let firstcode=code
-    cipher.split('').forEach((spell,index)=>{
-        console.log(spell,index,firstcode-1)
-        if(firstcode-1==index){
-            answer.push(spell)
-            firstcode+=code
-        }
-    })
-    return answer.join('');
+    var answer = cipher.split('').filter((v,i)=>(i+1)%code===0).join('');
+    
+    
+    return answer;
 }
+
+//기존코드
+// function solution(cipher, code) {
+//     var answer = [];
+//     let firstcode=code
+//     cipher.split('').forEach((spell,index)=>{
+//         console.log(spell,index,firstcode-1)
+//         if(firstcode-1==index){
+//             answer.push(spell)
+//             firstcode+=code
+//         }
+//     })
+//     return answer.join('');
+// }
