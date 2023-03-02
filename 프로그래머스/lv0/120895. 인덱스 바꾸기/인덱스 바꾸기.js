7e5a71f09c87724bc0849a8d4d6a66cfb5580b03
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2023-03-02 09:48:19 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,9 @@
+function solution(my_string, num1, num2) {
+
+    let answer=[...my_string]
+    answer.splice(num1,1,my_string[num2])
+    answer.splice(num2,1,my_string[num1])
+ 
+    
+    return answer.join('') ;
+}
