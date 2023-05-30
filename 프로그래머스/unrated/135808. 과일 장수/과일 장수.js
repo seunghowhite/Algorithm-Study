c59f40457019c840c37595cf9a4b3e41a9045d7d
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2023-05-30 21:27:09 +0900

    [unrated] Title: 과일 장수, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,10 +1,11 @@
-function solution(k, m, score) {
-    let answer = 0;
-    const sortApple =score.sort((a,b)=>b-a)
-    for(let i=0;i < sortApple.length;i+=m){
-        if(sortApple[i+m-1]){
-            answer +=sortApple[i+m-1]*m
-        }
-    }
-    return answer;
-}
+// function solution(k, m, score) {
+//     let answer = 0;
+//     const sortApple =score.sort((a,b)=>b-a)
+//     for(let i=0;i < sortApple.length;i+=m){
+//         if(sortApple[i+m-1]){
+//             answer +=sortApple[i+m-1]*m
+//         }
+//     }
+//     return answer;
+// }
+solution = (_, m, s) => s.sort().filter((_, i) => !((s.length - i) % m)).reduce((a, v) => a + v, 0) * m
