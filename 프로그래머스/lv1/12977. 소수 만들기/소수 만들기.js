39b445754b13bdd66b8e284bc8a28afab63b03d6
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2023-05-30 20:58:37 +0900

    [level 1] Title: 소수 만들기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,30 @@
+function solution(nums) {
+    let answer = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                const sum = nums[i] + nums[j] + nums[k];
+                if (isPrime(sum)) {
+                    answer++;
+                }
+            }
+        }
+    }
+    
+    return answer;
+}
+
+function isPrime(num) {
+    if (num < 2) {
+        return false;
+    }
+    
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    
+    return true;
+}
