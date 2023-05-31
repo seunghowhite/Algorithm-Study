fb850c642f4a8349b98949e472a02db9818f03c3
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2023-05-31 10:24:37 +0900

    [level 1] Title: 실패율, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,19 +1,30 @@
+// function solution(N, stages) {
+//     let pur = [];
+//     let count=stages.length
+//     let pursent={};
+//     for(let i=1;i<=N;i++){
+//         const stayUserNum=stages.filter((stage)=>stage===i)
+//         pur.push(stayUserNum.length)
+//     }
+//     for(let j=0;j<pur.length;j++){
+//         pursent[j+1]=pur[j]/(count)
+//         count-=pur[j]
+//     }
+
+//     const result = Object.entries(pursent)
+//       .sort((a, b) => b[1] - a[1])
+//       .map(entry => parseInt(entry[0]));
+//     return result
+
+// }
+
 function solution(N, stages) {
-    let pur = [];
-    let count=stages.length
-    let pursent={};
-    for(let i=1;i<=N;i++){
-        const stayUserNum=stages.filter((stage)=>stage===i)
-        pur.push(stayUserNum.length)
+    let result = [];
+    for(let i=1; i<=N; i++){
+        let reach = stages.filter((x) => x >= i).length;
+        let curr = stages.filter((x) => x === i).length;
+        result.push([i, curr/reach]);
     }
-    for(let j=0;j<pur.length;j++){
-        pursent[j+1]=pur[j]/(count)
-        count-=pur[j]
-    }
-
-    const result = Object.entries(pursent)
-      .sort((a, b) => b[1] - a[1])
-      .map(entry => parseInt(entry[0]));
-    return result
-
+    result.sort((a,b) => b[1] - a[1]);
+    return result.map((x) => x[0]);
 }
