c3a9721619fd98589e2816aeaa5509f54f7b2698
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2023-06-12 13:51:33 +0900

    [unrated] Title: 문자열 나누기, Time: 6.58 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
@@ -1,27 +1,54 @@
 function solution(s) {
-  let answer = 0;
-  let fristStr = "";
-  let outherStr = "";
-  for (let i = 0; i < s.length; i++) {
-    if (fristStr == "") {
-      fristStr = s[i];
-      continue;
-    }
-    if (s[i] === fristStr[0]) {
-      fristStr = fristStr + s[i];
-    } else if (s[i] !== fristStr[0]) {
-      outherStr += s[i];
-      if (fristStr.length === outherStr.length) {
-        fristStr = "";
-        outherStr = "";
-        answer++;
-      }
-    }
-  }
-  
-  if (fristStr !== "") {
-    answer++;
-  }
+    let answer =0;
+    let fristStr =""
+    let outherStr =""
+    for(let i=0; i<s.length;i++ ){
 
-  return answer;
+        if(fristStr==""){
+            fristStr = s[i]
+            continue
+        }
+        if (s[i]===fristStr[0]){
+           fristStr=fristStr+s[i]
+        }else if (s[i]!==fristStr[0]) {
+           outherStr+=s[i]
+        if(fristStr.length ===outherStr.length){
+            fristStr =""
+            outherStr=""
+            answer++
+     
+        }
+            // if(fristStr.length>0&&outherStr=="")
+            // answer++
+        }
+
+        
+    }
+    
+    return answer;
 }
+
+
+//
+// function solution(s) {
+//     let stack = [];
+//     let count = 0;
+    
+//     for(let i = 0; i < s.length; i += 1){
+//         stack.push(s[i]);
+        
+//         const same = stack.filter((item) => item === stack[0]);
+//         const notSame = stack.filter((item) => item !== stack[0]);
+        
+//         if(same.length === notSame.length){
+//             count += 1;
+//             stack = [];
+//         }
+//     }
+    
+//     if(stack.length !== 0){
+//         count += 1;
+//     }
+    
+//     return count;
+// }
