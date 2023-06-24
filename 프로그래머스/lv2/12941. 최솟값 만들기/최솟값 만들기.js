ec3622e515433e5c949fd698dcecaea1666cf648
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2023-06-24 11:56:11 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.72 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -2,9 +2,10 @@ function solution(A,B){
     let answer = 0;
     A.sort((a,b)=>a-b)
     B.sort((a,b)=>b-a)
-    for(let i=0; i<A.length;i++){
-       answer+= A[i]*B[i]
-    }
+    answer=A.reduce((total, val, idx) => total + val * B[idx], 0)
+    // for(let i=0; i<A.length;i++){
+    //    answer+= A[i]*B[i]
+    // }
 
     return answer;
 }
