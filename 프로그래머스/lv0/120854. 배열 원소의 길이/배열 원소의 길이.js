30cf9cce7a863b557a67d1f7ef53e9b5af7c87b0
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2023-07-26 09:06:30 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,9 +1,3 @@
 function solution(strlist) {
-    var answer = [];
-//     문자열의 겟수 알아보는 함수 찾기.
-// 그 리스트의 0~값부터 그 인덱스 값의 갯수 확인.
-    for(var i=0 ;i<strlist.length;i++){
-        answer[i] = strlist[i].length
-    }
-    return answer;
+    return strlist.map((el) => el.length)
 }
