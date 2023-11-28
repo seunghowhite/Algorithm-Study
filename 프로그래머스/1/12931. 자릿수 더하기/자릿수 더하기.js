11b11e9f39aa7024743543f83482fb2a2329ed44
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2023-11-28 12:48:06 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -0,0 +1,20 @@
+//2023년 11월 28일
+function solution(n){
+  return n.toString().split("").reduce((cur,acc)=>cur+Number(acc),0);
+}
+
+
+
+
+
+
+//이전코드
+// function solution(n)
+// {   let answer=0
+//     var StringNUM= String(n);
+//     let split = StringNUM.split('');   
+//     for(let i=0;i<split.length;i++){
+//         answer+=Number(split[i])
+//     }
+//     return answer;
+// }
