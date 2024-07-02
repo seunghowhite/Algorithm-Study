85145302e61fd7d2986177539d02020c9071593a
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2024-07-02 09:38:40 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,10 +1,4 @@
 // 2024-07-02
 function solution(n) {
-    var answer = 0;
-    if(n<7){
-        answer =1
-    }else {
-        answer = Math.ceil(n/7)
-    }
-    return answer;
-}
+    return Math.ceil(n / 7)
+};
