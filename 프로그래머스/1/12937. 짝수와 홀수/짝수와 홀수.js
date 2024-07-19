24e6477c7602d6d591e13dd51649f12972705d73
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2024-07-19 09:20:43 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -1,4 +1,17 @@
+//2024-07-19
 function solution(num) {
-    const answer = num%2==0?"Even":"Odd";
-    return answer;
+    return num%2?"Odd":"Even";
 }
+
+
+
+
+
+
+
+
+//2023-03-20
+// function solution(num) {
+//     const answer = num%2==0?"Even":"Odd";
+//     return answer;
+// }
