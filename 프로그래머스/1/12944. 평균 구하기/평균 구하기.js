4d1b004a9a98fdd1d8d6242c31b3940109c246f0
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2024-07-19 09:22:22 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -1,4 +1,18 @@
+//2024-07-19
 function solution(arr) {
-    let answer = arr.reduce((acc,cur)=>acc+cur)/arr.length;
-    return answer;
+    return arr.reduce((acc,cur)=>acc+cur)/arr.length;
 }
+
+
+
+
+
+
+
+
+
+//2023-03-20
+// function solution(arr) {
+//     let answer = arr.reduce((acc,cur)=>acc+cur)/arr.length;
+//     return answer;
+// }
