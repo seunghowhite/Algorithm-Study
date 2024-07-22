35682d6944b243ff2426312d97d5c4d7dc3d3649
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2024-07-22 09:53:11 +0900

    [level 1] Title: 약수의 합, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,8 +1,29 @@
-//2023년 11월 27일
-function solution(num) {
-    let sum = 0;
-    for (let i = 1; i <= num; i++) {
-        if (num % i === 0) sum += i
+//2024년 7월 22일
+function solution(n) {
+  let num = n;
+  let answer = 0
+  while(0 < n){
+    if(num % n === 0){
+        answer+=n
     }
-    return sum
+    n--
+  }
+    return answer
 }
+
+
+
+
+
+
+
+
+
+//2023년 11월 27일
+// function solution(num) {
+//     let sum = 0;
+//     for (let i = 1; i <= num; i++) {
+//         if (num % i === 0) sum += i
+//     }
+//     return sum
+// }
