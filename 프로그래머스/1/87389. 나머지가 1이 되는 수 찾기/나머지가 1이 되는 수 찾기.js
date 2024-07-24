b722d9ecce13f90a4879a577d07b9c47e80e89eb
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2024-07-24 09:28:44 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 5.23 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -0,0 +1,26 @@
+function solution(n) {
+    
+    for (let i=0; i<n; i++){
+       if(n%i===1){
+           return i
+       }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+//2023-03-20
+// function solution(n) {
+//     for(let i=2; i<n;i++){
+//         if(n%i ===1){
+//             return i
+//         }
+//     }
+// }
