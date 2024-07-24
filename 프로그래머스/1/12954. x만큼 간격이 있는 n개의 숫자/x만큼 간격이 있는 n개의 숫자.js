40f2e12710aad5c3feffaa26d7e80f3693a8833a
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2024-07-24 09:35:08 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.11 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.js
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.js
@@ -1,16 +1,36 @@
-// 내코드
+//2024-07-28
 function solution(x, n) {
     let answer = [];
-    for (let i=0;i<n;i++){
-        if(answer[i-1]==null){
-             answer.push(x) 
-        }else{
-             answer.push(answer[i-1]+x)
-        }
-       
+    let pushCount =x
+    while(answer.length!==n){
+        answer.push(pushCount)
+        pushCount+=x
     }
     return answer;
 }
+
+
+
+
+
+
+
+
+
+//2023-03-20
+// 내코드
+// function solution(x, n) {
+//     let answer = [];
+//     for (let i=0;i<n;i++){
+//         if(answer[i-1]==null){
+//              answer.push(x) 
+//         }else{
+//              answer.push(answer[i-1]+x)
+//         }
+       
+//     }
+//     return answer;
+// }
 
 // 다른사람 코드
 // function solution(x, n) {
