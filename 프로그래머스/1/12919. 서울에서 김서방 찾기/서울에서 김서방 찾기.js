b5c5486a4b0c147c8d3bd0cac27e757ec137827d
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2024-07-25 09:17:30 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 32.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -0,0 +1,20 @@
+//2024-07-25
+function solution(seoul) {
+    const answer =`김서방은 ${seoul.indexOf("Kim")}에 있다`
+    return answer;
+}
+
+
+
+
+
+
+
+
+//2023-03-20
+// function solution(seoul) {
+//     for(let i=0;i<=seoul.length;i++){
+//         if(seoul[i]=="Kim")return `김서방은 ${i}에 있다`;
+//     }
+
+// }
