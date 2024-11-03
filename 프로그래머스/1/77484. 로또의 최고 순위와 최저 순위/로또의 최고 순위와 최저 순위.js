e51666b9efc018d18a62bb0591e73b6e08cfd7e5
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2024-11-04 08:59:18 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.10 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -0,0 +1,81 @@
+//2024-11-04
+function solution(lottos, win_nums) {
+    var answer = [];
+    let sameCount =0
+    let zeroCount =0
+    lottos.forEach((num,index) => {
+        const isIncludes = win_nums.includes(num); 
+        if (isIncludes) { 
+            sameCount++;
+        }
+        if(num ===0){
+            zeroCount++
+        }
+        if(index ===lottos.length-1){
+            const max = sameCount+zeroCount
+            const min = sameCount
+         max > 1 ? answer.push(7 -   max) : answer.push(6);
+         min > 1 ? answer.push(7 -   min) : answer.push(6);
+        }
+    })
+    return answer;
+}
+
+
+
+
+
+
+
+
+
+//2023-03-20
+// function solution(lottos, win_nums) {
+//     var answer = [];
+//     let countmynum=0
+//     let lessnum=0
+    
+    
+    
+    
+//     lottos.forEach((mynum)=>{
+//         if(mynum==0){countmynum++}
+//         win_nums.forEach((winnum)=>{
+//             if(mynum==winnum){lessnum++}
+//         })
+//     });
+
+//     let choide =lessnum+countmynum
+
+    
+//     if(lessnum==6){
+//         answer.push(1)
+//     }else if(lessnum==5){
+//         answer.push(2)
+//     }else if(lessnum==4){
+//         answer.push(3)
+//     }else if(lessnum==3){
+//         answer.push(4)
+//     }else if(lessnum==2){
+//         answer.push(5)
+//     }else{
+//         answer.push(6)
+//     }
+    
+//     if(choide==6){
+//         answer.push(1)
+//     }else if(choide==5){
+//         answer.push(2)
+//     }else if(choide==4){
+//         answer.push(3)
+//     }else if(choide==3){
+//         answer.push(4)
+//     }else if(choide==2){
+//         answer.push(5)
+//     }else{
+//         answer.push(6)
+//     }
+    
+
+//     return answer.sort((a,b)=>a-b);
+// }
