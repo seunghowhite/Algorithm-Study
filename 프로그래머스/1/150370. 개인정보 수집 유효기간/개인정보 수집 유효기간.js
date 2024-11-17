2d5bc847538408b6b792bdc747bdc8e1dc16d445
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2024-11-17 09:42:40 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 47.20 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,20 +1,43 @@
+//2024-11-17
 function solution(today, terms, privacies) {
-  const answer = [];
-
-  const todatDate = new Date(today);
-
-  const termType = {};
-  terms.forEach((item) => {
-    const [type, term] = item.split(" ");
-    termType[type] = Number(term);
-  });
-
-  privacies.forEach((item, idx) => {
-    const [date, type] = item.split(" ");
-    const tableDate = new Date(date);
-    tableDate.setMonth(tableDate.getMonth() + termType[type]);
-    if (tableDate <= todatDate) answer.push(idx + 1);
-  });
-
-  return answer;
+    let result = [];
+    let obj1 = {};
+    for(let e of terms){
+        const [type, month] = e.split(" ");
+        obj1[type] = month;
+    }
+    for(let i = 0; i < privacies.length; i++){
+        const[start, type] = privacies[i].split(" ");
+        let now = new Date(start);
+        let date = new Date(today);
+        now.setMonth(now.getMonth() + Number(obj1[type]));
+        if(date >= now) result.push(i+1);
+    }
+    return result;
 }
+
+
+
+//2023-03-20
+// function solution(today, terms, privacies) {
+//   const answer = [];
+
+//   const todatDate = new Date(today);
+
+//   const termType = {};
+//   terms.forEach((item) => {
+//     const [type, term] = item.split(" ");
+//     termType[type] = Number(term);
+//   });
+
+//   privacies.forEach((item, idx) => {
+//     const [date, type] = item.split(" ");
+//     const tableDate = new Date(date);
+//     tableDate.setMonth(tableDate.getMonth() + termType[type]);
+//     if (tableDate <= todatDate) {
+//         answer.push(idx + 1)
+//     };
+//   });
+
+//   return answer;
+// }
