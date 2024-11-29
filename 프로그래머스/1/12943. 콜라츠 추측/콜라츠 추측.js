0199636fc1918ee5427e2065146c0e515258929a
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2024-11-29 17:57:22 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,20 +1,11 @@
 //2024년 7월 29일
-function solution(num, count = 0) {
-    // 종료조건1
-    if (num === 1) {
-        return count;
-    }
-    // 종료조건2
-    if (count >= 500) {
-        return -1;
-    }
-    // 현재 숫자가 짝수인 경우
-    if (num % 2 === 0) {
-        return solution(num / 2, count + 1);
-    } else {
-        // 현재 숫자가 홀수인 경우
-        return solution(num * 3 + 1, count + 1);
-    }
+function solution(num) {
+    let answer = 0;
+    while(num !=1 && answer !=500){
+        num%2==0 ? num = num/2 : num = num*3 +1;
+    answer++;
+  }
+    return num == 1 ? answer : -1;
 }
 
 
