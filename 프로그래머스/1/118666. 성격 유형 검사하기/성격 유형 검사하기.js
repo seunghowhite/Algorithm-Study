9ae8d459ac7ecf8682a1cceaceef52e3f5a24846
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2024-12-02 21:58:18 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.70 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,34 +1,46 @@
 function solution(survey, choices) {
-    let answer = '';
-    const choiceMap = {R:0,T:0,
-                       C:0,F:0,
-                       J:0,M:0,
-                       A:0,N:0}
-    for (let i = 0 ; i < survey.length; i++){
-        const choice = choices[i]
-        const result = Math.abs(choices[i] - 4);
-        const [leftType, rightType] = survey[i].split("");
-        const sur = survey[i]
-            if (choices[i] < 4) {
-              // 비동의 -> 왼쪽 요소
-              choiceMap[leftType] += result;
-            } else if (choices[i] > 4) {
-              // 동의 -> 오른쪽 원소
-              choiceMap[rightType] += result;
-            }
-        
+    const data = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 }
+
+    for (let i = 0; i < survey.length; i++) {
+        const score = choices[i] - 4
+        let type = survey[i].split('')[score < 0 ? 0 : 1] 
+        data[type] += Math.abs(score)
     }
-    
-    console.log(choiceMap)
-    const keys =Object.keys(choiceMap)
-    for(let j =0 ; j<keys.length; j+=2){
-        const L =choiceMap[keys[j]]
-        const R =choiceMap[keys[j+1]]
-        if(L>=R){
-            answer+=keys[j]
-        }else if(L<R){
-            answer+=keys[j+1]
-        }
-    }
-    return answer;
+
+    const { R, T, C, F, J, M, A, N } = data
+    return `${R >= T ? 'R' : 'T'}${C >= F ? 'C' : 'F'}${J >= M ? 'J' : 'M'}${A >= N ? 'A' : 'N'}`
 }
+// function solution(survey, choices) {
+//     let answer = '';
+//     const choiceMap = {R:0,T:0,
+//                        C:0,F:0,
+//                        J:0,M:0,
+//                        A:0,N:0}
+//     for (let i = 0 ; i < survey.length; i++){
+//         const choice = choices[i]
+//         const result = Math.abs(choices[i] - 4);
+//         const [leftType, rightType] = survey[i].split("");
+//         const sur = survey[i]
+//             if (choices[i] < 4) {
+//               // 비동의 -> 왼쪽 요소
+//               choiceMap[leftType] += result;
+//             } else if (choices[i] > 4) {
+//               // 동의 -> 오른쪽 원소
+//               choiceMap[rightType] += result;
+//             }
+        
+//     }
+    
+//     console.log(choiceMap)
+//     const keys =Object.keys(choiceMap)
+//     for(let j =0 ; j<keys.length; j+=2){
+//         const L =choiceMap[keys[j]]
+//         const R =choiceMap[keys[j+1]]
+//         if(L>=R){
+//             answer+=keys[j]
+//         }else if(L<R){
+//             answer+=keys[j+1]
+//         }
+//     }
+//     return answer;
+// }
