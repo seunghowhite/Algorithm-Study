09b0fc41dcb0f8ee5ea463a8597ab08ff70dd869
Seung Ho <qortmdgh0320@gmail.com>
2025-07-28 09:26:09 +0900

    [level 0] Title: 카운트 업, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.js
@@ -0,0 +1,9 @@
+function solution(start_num, end_num) {
+    var answer = [];
+    const increse = end_num-start_num
+    for(let i= start_num; i<=end_num;i++){
+        answer.push(i)
+    }
+    
+    return answer;
+}
