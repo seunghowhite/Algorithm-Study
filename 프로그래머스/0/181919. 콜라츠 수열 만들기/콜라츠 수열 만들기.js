c40d1ed6e2e84ba613f6649487f60bbfbce1c39e
Seung Ho <qortmdgh0320@gmail.com>
2025-07-29 09:10:33 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,9 @@
+function solution(n,arr=[]) {
+    arr.push(n);
+    if (n === 1) return arr;
+    if (n % 2 === 0) {
+        return solution(n / 2, arr);
+    } else {
+        return solution(3 * n + 1, arr);
+    }
+}
